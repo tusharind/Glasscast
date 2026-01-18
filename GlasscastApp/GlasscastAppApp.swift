c2897d30de15bc24cd4e88ca089eb2c60c2563ee
Tushar <tusharj3255@gmail.com>
2026-01-18 15:45:43 +0530

    Initial Commit

---
+++ GlasscastApp/GlasscastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastAppApp.swift
+//  GlasscastApp
+//
+//  Created by Prakhar Jaiswal on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
